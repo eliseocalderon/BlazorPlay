bb283d8dd7edc8f6156e3ed347ba659ed905023a
Eliseo Calderon <eliseo.calderon@gmail.com>
2019-10-24 18:06:17 -0500

    Add Person card  page and component.

---
+++ BlazorPlay/Server/Controllers/PersonController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlazorPlay.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorPlay.Server.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        [HttpGet]
+        [Route("all")]
+        public Task<IEnumerable<Person>> GetPeople()
+        {
+            var result = new List<Person>
+            {
+                new Person {FirstName = "Tim", LastName="Corey", AccountBalance=19.45M},
+                new Person {FirstName = "Mary", LastName="Jones", AccountBalance=105.87M},
+                new Person {FirstName = "John", LastName="Smith", AccountBalance=115.32M}
+            };
+            return Task.FromResult(result as IEnumerable<Person>);
+        }
+
+        [HttpPost]
+        public async Task Post(Person person)
+        {
+
+        }
+    }
+}
---
+++ BlazorPlay/Shared/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorPlay.Shared
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public decimal AccountBalance { get; set; }
+        public string FullName => $"{FirstName} {LastName}";
+    }
+}
