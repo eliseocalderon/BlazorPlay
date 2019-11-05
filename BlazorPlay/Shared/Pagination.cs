105b9cf178847718952a2720d08c91a03bd9f97f
Eliseo Calderon <ecalderon@Nerium.com>
2019-11-05 12:52:52 -0600

    Added People card list Upgraded to 3.1 preview 2

---
+++ BlazorPlay/Shared/Pagination.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorPlay.Shared
+{
+    public class Pagination
+    {
+        public int Page { get; set; } = 1;
+        public int QuantityPerPage { get; set; } = 10;
+    }
+}
